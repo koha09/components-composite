9843de295a1d355e1cea7834026e8f0c31461980
mrikaev <mrikaev@mwpost.ru>
2020-09-12 18:43:40 +0300

    Правки

---
+++ templates/constructor.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Guardian
+ * Date: 10.09.2020
+ * Time: 17:55
+ */
+
+?>
+
+<h1>Конфигуратор компьютера</h1>
+<div>
+
+</div>
+<h2>Системный блок</h2>
+<div id="configuration">
+
+</div>
