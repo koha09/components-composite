e764e49791f820547e7368b47faa46b845827d17
mrikaev <mrikaev@mwpost.ru>
2020-09-16 15:15:21 +0300

    Подключение redux и redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Configurator from './components/Configurator'
+import { Provider } from 'react-redux'
+import { applyMiddleware, compose, createStore } from 'redux'
+import rootReducer from './redux/rootReducer'
+import thunk from 'redux-thunk' 
+import { fetchCategories } from './redux/actions'
+
+const store = createStore(rootReducer, compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    )
+store.dispatch(fetchCategories())
 
 ReactDOM.render(
-    <Configurator/>,
+    <Provider store={store}>
+        <Configurator/>
+    </Provider>,
     document.getElementById('configuration')
 )
